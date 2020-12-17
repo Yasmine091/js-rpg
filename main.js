938392e91cba3e2dd6539867562f0c6121d380f7
Yasmine091 <yasmine.emdounan@gmail.com>
2020-12-17 20:06:57 +0100

    battle simulation + stats

--- main.js
+++ main.js
@@ -3,52 +3,47 @@ import Elf from './lib/races/Elf.js'
 import Dwarf from './lib/races/Dwarf.js'
 import Hero from './lib/Hero.js';
 import BattleSimulation from './lib/BattleSim.js';
+import { clearSimulation, clearStats } from '../../lib/functions.js';
+
 /* import Enemy from './lib/Enemy.js';
 import Battle from './lib/Battle.js'; */
 
 class Game {
-    constructor(name, race, enemy){
+    constructor(name, race, enemy) {
         this.hero = new Hero(name, race);
         this.enemy = enemy;
     }
 
-    
+
 }
 
-let hero = new Elf("Kuki", "Elf");
-let enemy = new Human("Yasmine", "Human");
-let enemy2 = new Dwarf("Yasou", "Dwarf");
-
-/* 
-enemy.setHitStrength();
-console.log(enemy.attack()); */
-
-/*
--> Monter de niveau ou gagner de l'exp :
-    console.log(enemy.setLvl());
-    console.log(enemy.setXp()); 
-*/
-
-let simBattle1 = new BattleSimulation(hero, enemy2);
-
-console.log(simBattle1.p1Stats());
-console.log(simBattle1.p2Stats());
-
-//console.log(simBattle1.p1Hit());
-//console.log(simBattle1.p2Hit());
-
-console.log(simBattle1.HeroVsEnemies());
-//console.log(simBattle1.EnemyVsEnemy());
-
-console.log(simBattle1.p1Stats());
-console.log(simBattle1.p2Stats());
-
+let running = false;
 
 document.addEventListener('submit', () => {
     event.preventDefault();
     let name = document.getElementById('name').value;
+
+    let player1 = new Elf(name, "Elf");
+    let player2 = new Dwarf("Machin", "Dwarf");
+
+
+    let simBattle = new BattleSimulation(player1, player2);
+
+
+
+    if (running === false) {
+
+        running = true;
+        clearStats("p1-Stats");
+        clearStats("p2-Stats");
+        clearSimulation();
+        simBattle.HeroVsEnemies();
+        //simBattle1.EnemyVsEnemy();
+
+    }
+
     //let race = document.getElementById('race').value;
-    
-    let game = new Game(name, "Elf", "Dragon");
-    console.log(game);
+
+    /* let game = new Game(name, "Elf", "Dragon");
+    console.log(game); */
 });
